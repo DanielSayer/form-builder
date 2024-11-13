cf0d160e3c43c8a33e6c003eeea42d17304d209a
Daniel Sayer <daniel.sayer01@gmail.com>
2024-11-13 21:35:43 +1000

    feat: add a color picker

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,21 +1,39 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light" | "system";
+const DEFAULT_COLOR = "zinc";
+export const THEME_COLORS = [
+  { name: "blue", value: "#2563EB" },
+  { name: "green", value: "#16A34A" },
+  { name: "orange", value: "#F97316" },
+  { name: "red", value: "#DC2626" },
+  { name: "rose", value: "#E11D48" },
+  { name: "violet", value: "#7C3AED" },
+  { name: "yellow", value: "#FACC15" },
+  { name: "zinc", value: "#18181B" },
+] as const;
+
+type BaseTheme = "dark" | "light" | "system";
+type ColorTheme = (typeof THEME_COLORS)[number]["name"];
 
 type ThemeProviderProps = {
   children: React.ReactNode;
-  defaultTheme?: Theme;
+  defaultTheme?: BaseTheme;
+  defaultColor?: ColorTheme;
   storageKey?: string;
 };
 
 type ThemeProviderState = {
-  theme: Theme;
-  setTheme: (theme: Theme) => void;
+  theme: BaseTheme;
+  color: ColorTheme;
+  setTheme: (theme: BaseTheme) => void;
+  setColor: (color: ColorTheme) => void;
 };
 
 const initialState: ThemeProviderState = {
   theme: "system",
+  color: DEFAULT_COLOR,
   setTheme: () => null,
+  setColor: () => null,
 };
 
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
@@ -23,17 +41,35 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 export function ThemeProvider({
   children,
   defaultTheme = "system",
+  defaultColor = DEFAULT_COLOR,
   storageKey = "form-builder-theme",
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
+  const [theme, setTheme] = useState<BaseTheme>(
+    () =>
+      (localStorage.getItem(`${storageKey}-mode`) as BaseTheme) || defaultTheme,
+  );
+
+  const [color, setColor] = useState<ColorTheme>(
+    () =>
+      (localStorage.getItem(`${storageKey}-color`) as ColorTheme) ||
+      defaultColor,
   );
 
   useEffect(() => {
     const root = window.document.documentElement;
 
     root.classList.remove("light", "dark");
+    root.classList.remove(
+      "blue",
+      "green",
+      "orange",
+      "red",
+      "rose",
+      "violet",
+      "yellow",
+      "zinc",
+    );
 
     if (theme === "system") {
       const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
@@ -45,14 +81,35 @@ export function ThemeProvider({
       return;
     }
 
+    root.classList.add(color);
     root.classList.add(theme);
+  }, [theme, color]);
+
+  useEffect(() => {
+    if (theme !== "system") return;
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChange = () => {
+      const root = window.document.documentElement;
+      root.classList.remove("light", "dark");
+      root.classList.add(mediaQuery.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
   }, [theme]);
 
   const value = {
     theme,
-    setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme);
+    color,
+    setTheme: (theme: BaseTheme) => {
+      localStorage.setItem(`${storageKey}-mode`, theme);
       setTheme(theme);
+    },
+    setColor: (color: ColorTheme) => {
+      localStorage.setItem(`${storageKey}-color`, color);
+      setColor(color);
     },
   };
 
--- src/pages/settings/appearance/page.tsx
+++ src/pages/settings/appearance/page.tsx
@@ -1,72 +1,107 @@
-import { useTheme } from "@/components/providers/theme-provider";
+import { THEME_COLORS, useTheme } from "@/components/providers/theme-provider";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
 
 export function AppearancePage() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, color, setColor } = useTheme();
+  console.log(color);
 
   return (
-    <div className="space-y-1">
-      <Label htmlFor="theme">Theme</Label>
-      <p className="text-sm text-muted-foreground">
-        This won't change how the code is generated, but you can change your
-        theme for the form builder here.
-      </p>
-      <RadioGroup
-        id="theme"
-        onValueChange={setTheme}
-        defaultValue={theme}
-        className="grid max-w-md grid-cols-2 gap-8 pt-2"
-      >
-        <div>
-          <Label className="[&:has([data-state=checked])>div]:border-primary">
-            <RadioGroupItem value="light" className="sr-only" />
-            <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-              <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                  <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                </div>
-                <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                  <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                </div>
-                <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                  <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                  <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                </div>
-              </div>
-            </div>
-            <span className="block w-full p-2 text-center font-normal">
-              Light
-            </span>
-          </Label>
-        </div>
-        <div>
-          <Label className="[&:has([data-state=checked])>div]:border-primary">
-            <RadioGroupItem value="dark" className="sr-only" />
-            <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-              <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                  <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                </div>
-                <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                  <div className="h-4 w-4 rounded-full bg-slate-400" />
-                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                </div>
-                <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                  <div className="h-4 w-4 rounded-full bg-slate-400" />
-                  <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Appearance</h3>
+        <p className="text-sm text-muted-foreground">
+          Select the theme of your app, and you can better picture how the forms
+          will look.
+        </p>
+      </div>
+      <Separator />
+      <div className="space-y-1">
+        <Label htmlFor="theme">Theme</Label>
+        <p className="text-sm text-muted-foreground">
+          This won't change how the code is generated, but you can change your
+          theme for the form builder here.
+        </p>
+        <RadioGroup
+          id="theme"
+          onValueChange={setTheme}
+          defaultValue={theme}
+          className="grid max-w-md grid-cols-2 gap-8 pt-2"
+        >
+          <div>
+            <Label className="[&:has([data-state=checked])>div]:border-primary">
+              <RadioGroupItem value="light" className="sr-only" />
+              <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
+                <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
+                  <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                    <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                  </div>
                 </div>
               </div>
-            </div>
-            <span className="block w-full p-2 text-center font-normal">
-              Dark
-            </span>
-          </Label>
-        </div>
-      </RadioGroup>
+              <span className="block w-full p-2 text-center font-normal">
+                Light
+              </span>
+            </Label>
+          </div>
+          <div>
+            <Label className="[&:has([data-state=checked])>div]:border-primary">
+              <RadioGroupItem value="dark" className="sr-only" />
+              <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
+                <div className="space-y-2 rounded-sm bg-slate-950 p-2">
+                  <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                    <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
+                    <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                    <div className="h-4 w-4 rounded-full bg-slate-400" />
+                    <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                  </div>
+                  <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                    <div className="h-4 w-4 rounded-full bg-slate-400" />
+                    <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                  </div>
+                </div>
+              </div>
+              <span className="block w-full p-2 text-center font-normal">
+                Dark
+              </span>
+            </Label>
+          </div>
+        </RadioGroup>
+      </div>
+      <div>
+        <RadioGroup
+          className="grid grid-cols-2 gap-4 md:grid-cols-4"
+          defaultValue={color}
+          onValueChange={setColor}
+        >
+          {THEME_COLORS.map((color) => (
+            <Label
+              className="[&:has([data-state=checked])>div]:border-primary"
+              key={color.name}
+            >
+              <RadioGroupItem value={color.name} className="sr-only" />
+              <div className="flex items-center gap-4 rounded-lg border bg-popover p-4 text-card-foreground shadow-sm">
+                <div
+                  className={`h-6 w-6 rounded-full`}
+                  style={{ backgroundColor: color.value }}
+                ></div>
+                <div>{color.name}</div>
+              </div>
+            </Label>
+          ))}
+        </RadioGroup>
+      </div>
     </div>
   );
 }
